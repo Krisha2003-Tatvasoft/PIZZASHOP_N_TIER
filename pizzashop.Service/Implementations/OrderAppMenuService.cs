04962064708cf5d4a490c4b839e7693584546a87
krisha2003-Tatvasoft <tatva.pci44@outlook.com>
2025-05-19 23:48:08 +0530

    Testing

--- pizzashop.Service/Implementations/OrderAppMenuService.cs
+++ pizzashop.Service/Implementations/OrderAppMenuService.cs
@@ -447,9 +447,10 @@ public class OrderAppMenuService : IOrderAppMenuService
     public async Task<bool> checkOrderStatus(int id)
     {
         var order = await _orderRepository.OrderDetailsByIdAsync(id);
+         Console.WriteLine("hellllllllllllllllllllllo" + order.status);
         if (order != null)
         {
-            if (order.status == 1 || order.status == 2)
+            if (order.status == 0 || order.status == 1)
             {
                 return true;
             }
